ab38ba60e7c98026db457534723cad9a5cd20675
Sibongumusa Lungelo <81442054+S-codes14@users.noreply.github.com>
2022-03-14 12:47:38 +0200

    template MUI clock design
    It would be great if time actually looked like this
    but I really don't know maybe i should continue though

--- src/time/Time.js
+++ src/time/Time.js
@@ -1,86 +1,108 @@
-import React, { useState, useEffect, useRef } from 'react'
-import styles from './styles.module.css'
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticTimePicker from '@mui/lab/StaticTimePicker';
 
-function Time({
-  year,
-  month,
-  day,
-  setDay,
-  selectedMinute,
-  setSelectedMinute,
-  selectedHour,
-  setSelectedHour,
-  minuteInterval = 5,
-  disabledHours = [],
-  disabledMinutes = [],
-  BGColor = 'rgb(44, 76, 104)',
-}) {
-  const hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
-
-  const createMinuteList = (interval) => {
-    var list = [];
-    for (var i = 0; i < 60; i++) {
-      if (i % interval === 0) {
-        list.push(i.toString().length > 1 ? i.toString() : `0${i}`);
-      }
-    }
-    return list;
-  }
-
-  const [minutes, setMinutes] = useState(createMinuteList(minuteInterval));
-
-  const useDidMountEffect = (func, deps) => {
-      const didMount = useRef(false);
-
-      useEffect(() => {
-          if (didMount.current) func();
-          else didMount.current = true;
-      }, deps);
-  }
-
-
-  useDidMountEffect(() => {
-    setDay(new Date(year, month, day.getDate(), selectedHour, selectedMinute));
-  }, [selectedHour, selectedMinute]);
+export default function Time() {
+  const [value, setValue] = React.useState(new Date());
 
   return (
-    <div className={styles.timeContainer} style={{ backgroundColor : BGColor}}>
-     <div className={styles.timeHours}>
-        {
-          hours.reverse().map((hour, i) => {
-            if (hour === selectedHour) {
-              return <div className={styles.selectedHourDiv} key={i}>{hour}</div>
-            }
-            else if (disabledHours.includes(hour)) {
-              return <div className={styles.disabledHourDiv} key={i}>{hour}</div>
-            }
-            else {
-              return <div className={styles.hourDiv} key={i} onClick={() => {
-                setSelectedHour(hour);
-              }}>{hour}</div>
-            }
-          })
-        }
-     </div>
-     <div className={styles.timeMinutes}>
-        {
-          minutes.map((minute, i) => {
-            if (minute === selectedMinute) {
-              return <div className={styles.selectedMinuteDiv} key={i}>{minute}</div>
-            }
-            else if (disabledMinutes.includes(minute)) {
-              return <div className={styles.disabledMinuteDiv} key={i}>{minute}</div>
-            }
-            else {
-              return <div className={styles.minuteDiv} key={i} onClick={() => {
-                setSelectedMinute(minute);
-              }}>{minute}</div>
-            }
-          })
-        }
-     </div>
-    </div>
-  )
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticTimePicker
+        displayStaticWrapperAs="mobile"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
 }
+// import React, { useState, useEffect, useRef } from 'react'
+// import styles from './styles.module.css'
 
-export default Time
+// function Time({
+//   year,
+//   month,
+//   day,
+//   setDay,
+//   selectedMinute,
+//   setSelectedMinute,
+//   selectedHour,
+//   setSelectedHour,
+//   minuteInterval = 5,
+//   disabledHours = [],
+//   disabledMinutes = [],
+//   BGColor = 'rgb(44, 76, 104)',
+// }) {
+//   const hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+
+//   const createMinuteList = (interval) => {
+//     var list = [];
+//     for (var i = 0; i < 60; i++) {
+//       if (i % interval === 0) {
+//         list.push(i.toString().length > 1 ? i.toString() : `0${i}`);
+//       }
+//     }
+//     return list;
+//   }
+
+//   const [minutes, setMinutes] = useState(createMinuteList(minuteInterval));
+
+//   const useDidMountEffect = (func, deps) => {
+//       const didMount = useRef(false);
+
+//       useEffect(() => {
+//           if (didMount.current) func();
+//           else didMount.current = true;
+//       }, deps);
+//   }
+
+
+//   useDidMountEffect(() => {
+//     setDay(new Date(year, month, day.getDate(), selectedHour, selectedMinute));
+//   }, [selectedHour, selectedMinute]);
+
+//   return (
+//     <div className={styles.timeContainer} style={{ backgroundColor : BGColor}}>
+//      <div className={styles.timeHours}>
+//         {
+//           hours.reverse().map((hour, i) => {
+//             if (hour === selectedHour) {
+//               return <div className={styles.selectedHourDiv} key={i}>{hour}</div>
+//             }
+//             else if (disabledHours.includes(hour)) {
+//               return <div className={styles.disabledHourDiv} key={i}>{hour}</div>
+//             }
+//             else {
+//               return <div className={styles.hourDiv} key={i} onClick={() => {
+//                 setSelectedHour(hour);
+//               }}>{hour}</div>
+//             }
+//           })
+//         }
+//      </div>
+//      <div className={styles.timeMinutes}>
+//         {
+//           minutes.map((minute, i) => {
+//             if (minute === selectedMinute) {
+//               return <div className={styles.selectedMinuteDiv} key={i}>{minute}</div>
+//             }
+//             else if (disabledMinutes.includes(minute)) {
+//               return <div className={styles.disabledMinuteDiv} key={i}>{minute}</div>
+//             }
+//             else {
+//               return <div className={styles.minuteDiv} key={i} onClick={() => {
+//                 setSelectedMinute(minute);
+//               }}>{minute}</div>
+//             }
+//           })
+//         }
+//      </div>
+//     </div>
+//   )
+// }
+
+// export default Time
